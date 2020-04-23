139a530956664b741877dff236df842e6a3b356f
bjornbrodtkorb <bjorn.thoralf@outlook.com>
2020-04-23 13:51:42 +0200

    FAT: Worked on the new FAT32 library

--- Strawberry/main.c
+++ Strawberry/main.c
@@ -39,71 +39,14 @@ void runtime_stats(void* arg)
 //--------------------------------------------------------------------------------------------------//
 
 
-void welcome_thread(void* arg)
-{
-	// Print a happy message to the screen
-	board_serial_print("Kernel successfully started\n\n");
-}
-
-
-void wav1(void* arg)
-{
-	gpio_set_pin_function(PIOB, 2, PERIPHERAL_FUNCTION_OFF);
-	gpio_set_pin_direction_output(PIOB, 2);
-	while (1)
-	{
-		gpio_toogle_pin_value(PIOB, 2);
-		syscall_sleep(1);
-	}
-}
-
-
-void wav2(void* arg)
-{
-	gpio_set_pin_function(PIOB, 3, PERIPHERAL_FUNCTION_OFF);
-	gpio_set_pin_direction_output(PIOB, 3);
-	while (1)
-	{
-		gpio_toogle_pin_value(PIOB, 3);
-		syscall_sleep(1);
-	}
-}
-
-
-void wav3(void* arg)
-{
-	gpio_set_pin_function(PIOA, 0, PERIPHERAL_FUNCTION_OFF);
-	gpio_set_pin_direction_output(PIOA, 0);
-	while (1)
-	{
-		gpio_toogle_pin_value(PIOA, 0);
-		syscall_sleep(1);
-	}
-}
-
-//--------------------------------------------------------------------------------------------------//
-
-
 int main(void)
 {
 	// This functions starts up the kernel and initializes the basic drivers
 	kernel_startup();
-	board_sd_card_config();
-	board_serial_print("Heisann\n");
-	/*
-	uint8_t retry_count = 0;
-	file_system_t cortex_file_system;
-	while (retry_count++ < 5)
-	{
-		if (file_mount(&cortex_file_system, "", 1) == FR_OK)
-		{
-			break;
-		}
-
-	}
-	*/
 	
-	fat_config();
+	board_serial_print("BIOS success\n\n");
+	
+	// Test the FAT32 file system
 	
 	while (1);
 }
